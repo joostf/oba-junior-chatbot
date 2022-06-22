77b9873ac8f1a4357b1d5091bbf33a5b54e60e9e
MUHAMMET KOMUR <muhammet075@icloud.com>
2022-06-22 10:44:39 +0200

    sound changes

--- public/sw.js
+++ public/sw.js
@@ -1,62 +1,62 @@
-// when static  files are changed update them under a new cache name
-const staticCacheName = "site-static-v5";
-// const dynamicCacheName = "site-dynamic-v1";
-const assets = ["/css/index.css", "/html/offline.html"];
+// // when static  files are changed update them under a new cache name
+// const staticCacheName = "site-static-v5";
+// // const dynamicCacheName = "site-dynamic-v1";
+// const assets = ["/css/index.css", "/html/offline.html"];
 
-// LIMIT CACHE SIZE
-// const limitCacheSize = (name, size) => {
-//   caches.open(name).then((cache) => {
-//     cache.keys().then((keys) => {
-//       if (keys.length > size) {
-//         cache.delete(keys[0]).then(limitCacheSize(name, size));
-//       }
-//     });
-//   });
-// };
+// // LIMIT CACHE SIZE
+// // const limitCacheSize = (name, size) => {
+// //   caches.open(name).then((cache) => {
+// //     cache.keys().then((keys) => {
+// //       if (keys.length > size) {
+// //         cache.delete(keys[0]).then(limitCacheSize(name, size));
+// //       }
+// //     });
+// //   });
+// // };
 
-// INSTALL
-// check if serviceWorker is intalled
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      cache.addAll(assets);
-    })
-  );
-});
+// // INSTALL
+// // check if serviceWorker is intalled
+// self.addEventListener("install", (evt) => {
+//   evt.waitUntil(
+//     caches.open(staticCacheName).then((cache) => {
+//       cache.addAll(assets);
+//     })
+//   );
+// });
 
-// ACTIVATE
-// activate serviceWorker
-self.addEventListener("activate", (evt) => {
-  evt.waitUntil(
-    // when there is a new cacheName delete the old one
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => key !== staticCacheName)
-          .map((key) => caches.delete(key))
-      );
-    })
-  );
-});
+// // ACTIVATE
+// // activate serviceWorker
+// self.addEventListener("activate", (evt) => {
+//   evt.waitUntil(
+//     // when there is a new cacheName delete the old one
+//     caches.keys().then((keys) => {
+//       return Promise.all(
+//         keys
+//           .filter((key) => key !== staticCacheName)
+//           .map((key) => caches.delete(key))
+//       );
+//     })
+//   );
+// });
 
-// FETCH EVENTS
-self.addEventListener("fetch", (evt) => {
-  // interupt the request before it goes to the server
-  evt.respondWith(
-    // get assets from cache (otherwise make the fetch from the server)
-    caches.match(evt.request).then((cacheRes) => {
-      return (
-        cacheRes ||
-        fetch(evt.request)
-          //   .then((fetchRes) => {
-          //     return caches.open(dynamicCacheName).then((cache) => {
-          //       cache.put(evt.request.url, fetchRes.clone());
-          //       limitCacheSize(dynamicCacheName, 10);
-          //       return fetchRes;
-          //     });
-          //   })
-          .catch(() => caches.match("/html/offline.html"))
-      );
-    })
-  );
-});
+// // FETCH EVENTS
+// self.addEventListener("fetch", (evt) => {
+//   // interupt the request before it goes to the server
+//   evt.respondWith(
+//     // get assets from cache (otherwise make the fetch from the server)
+//     caches.match(evt.request).then((cacheRes) => {
+//       return (
+//         cacheRes ||
+//         fetch(evt.request)
+//           //   .then((fetchRes) => {
+//           //     return caches.open(dynamicCacheName).then((cache) => {
+//           //       cache.put(evt.request.url, fetchRes.clone());
+//           //       limitCacheSize(dynamicCacheName, 10);
+//           //       return fetchRes;
+//           //     });
+//           //   })
+//           .catch(() => caches.match("/html/offline.html"))
+//       );
+//     })
+//   );
+// });
